dac3c8ab82db38055f8385749a34663e354122ec
Eric <ebagnaro@gmail.com>
2023-12-13 13:36:24 +0100

    - Creating and combining views

---
+++ Landmarks2023/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks2023
+//
+//  Created by Eric on 13/12/2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
