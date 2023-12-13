22f5d0977de207ff0bf3c660e7a1a075d414b274
Eric <ebagnaro@gmail.com>
2023-12-13 13:17:12 +0100

    Initial Commit

---
+++ Landmarks2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks2023
+//
+//  Created by Eric on 13/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks2023/Landmarks2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks2023App.swift
+//  Landmarks2023
+//
+//  Created by Eric on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
