3bea0999ee39c2af7479d67d43004f0b5974a786
Eric <ebagnaro@gmail.com>
2023-12-14 14:50:39 +0100

    Animating views and transitions

---
+++ Landmarks2023/Model/Hike.swift
@@ -0,0 +1,31 @@
+//
+//  Hike.swift
+//  Landmarks2023
+//
+//  Created by Eric on 14/12/2023.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+    
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+    
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var pace: Range<Double>
+        var elevation: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
