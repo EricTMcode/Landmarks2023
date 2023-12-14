c2e84406af9c022c712b46c818f4b8ab2954cb2b
Eric <ebagnaro@gmail.com>
2023-12-14 14:06:58 +0100

    - Drawing paths and shapes

---
+++ Landmarks2023/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks2023
+//
+//  Created by Eric on 14/12/2023.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
