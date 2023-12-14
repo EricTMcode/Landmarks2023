c08f9fdd9ed8d6484616e023392042ca4e324b87
Eric <ebagnaro@gmail.com>
2023-12-14 15:16:13 +0100

    - Composing complex interfaces

---
+++ Landmarks2023/Views/Categories/CategoryHome.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryHome.swift
+//  Landmarks2023
+//
+//  Created by Eric on 14/12/2023.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
